085a27e6cd2597075384d7aeec2133a618948761
Vitalij <vitalijselynin@msn.com>
2018-11-03 15:46:15 +0100

    Set baseline to passive bot
    Passive bot used for further development / comparison with own trained bot

--- SVM/MyBot.py
+++ SVM/MyBot.py
@@ -3,11 +3,11 @@
 import SVMBot
 
 
-class SVMBotAggressive(SVMBot.SVMBot):
+class SVMBotPassive(SVMBot.SVMBot):
     def __init__(self):
         super().__init__("MyBot.svc")
 
 
 if __name__ == '__main__':
-    bot = SVMBotAggressive()
+    bot = SVMBotPassive()
     bot.run()
